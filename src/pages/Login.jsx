d86596ebacfde84edb36df23968a37c22fa7bb5b
Subny <richsubin411@gmail.com>
2023-08-06 00:41:16 +0900

    [FEAT] 로그인 페이지 - #3 로그인 페이지 1차 완료 - #3 로그인 관련 이미지들 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,32 @@
 import React from "react";
-import {Link, useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import '../pages/css/login.css'
+import '../pages/css/common.css'
 
 const Login = () => {
+    
     return(
-        <div className='login'>
-            <div className='loginBox'>
+        <div className='backPage'>
+            <div className='contentBox'>
+                <img src={`${process.env.PUBLIC_URL}/assets/images/GoLAB.png`} alt="고랩" className="goLab"/>
                 <div>
                 <input type="text" className= "iptBox" placeholder="아이디" required/>
                 </div>
                 <div>
                 <input type="password" className= "iptBox pwBox" placeholder="비밀번호" required/>
                 </div>
-                <div>
-                <input type="checkbox" className='autoLogin'/><label for="autoLogin" className='autoLogin'>자동 로그인</label>
+                <div className='autoLogin'>
+                <input type="checkbox"/><label for="autoLogin">자동 로그인</label>
                 </div>
-                <button id="loginBtn">로그인</button>
-                <div className="loginIcons"></div>
+                <div>
+                    <a href="http://localhost:8080/login/social/naver"><img src={`${process.env.PUBLIC_URL}/assets/images/login/naver.png`} className="loginIcons" alt="네이버로그인"/></a>
+                    <a href="http://localhost:8080/login/social/google"><img src={`${process.env.PUBLIC_URL}/assets/images/login/google.png`} className="loginIcons" alt="구글로그인"/></a><br/>
+                    <a href="http://localhost:8080/login/social/kakao"><img src={`${process.env.PUBLIC_URL}/assets/images/login/kakao.png`} className="loginIcons" alt="카카오로그인"/></a>
+                    <Link to = {"/login"}><img src={`${process.env.PUBLIC_URL}/assets/images/login/loginButton.png`} className="loginIcons" alt="일반로그인"/></Link>
+                </div>
                 <Link to = {"/find-id"}><p className="linkText" id="idText">아이디찾기</p></Link>
                 <Link to = {"/find-pw"}><p className="linkText"id="pwText">비밀번호찾기</p></Link>
-                <Link to = {"/join"}><p className="linkText" id="joinText">회원가입</p></Link>
+                <Link to = {"/join"}><p className="linkText" id="joinText">아직 계정이 없으신가요?</p></Link>
             </div>
         </div>
     )
